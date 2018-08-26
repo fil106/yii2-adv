c050c4c8942cc8e0376bf85ea2f3234fb49fdadf
Filipp <fil106@mail.ru>
2018-08-26 22:41:51 +0300

    Create MyTest

---
+++ frontend/tests/unit/MyTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace frontend\tests;
+
+class MyTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \frontend\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    	$this->assertTrue(1,1);
+
+    }
+}
