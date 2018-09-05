de1da53fa4105c91b5b4a79a34571e640485fd74
Filipp Fedorov <fil106@mail.ru>
2018-09-06 00:38:35 +0300

    Copy from dmstr layout

---
+++ backend/views/layouts/adminlte/header.php
@@ -0,0 +1,67 @@
+<?php
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+/* @var $user \common\models\User */
+
+$user = Yii::$app->user->identity;
+?>
+
+<header class="main-header">
+
+    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', '/', ['class' => 'logo']) ?>
+
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+
+                <!-- User Account: style can be found in dropdown.less -->
+
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+                        <?= Html::img($user->getThumbUploadUrl('avatar', \common\models\User::AVATAR_ICO), ['class' => 'user-image']) ?>
+
+                        <span class="hidden-xs"><?= $user->username ?></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <?= Html::img($user->getThumbUploadUrl('avatar', \common\models\User::AVATAR_MIDDLE), ['class' => 'img-circle']) ?>
+                            <p>
+                                <?= $user->username ?>
+                                <small>Member since <?= Yii::$app->formatter->asDate($user->created_at) ?></small>
+                            </p>
+                        </li>
+
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="/user/update/<?= $user->getId() ?>" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+                            <div class="pull-right">
+                                <?= Html::a(
+                                    'Sign out',
+                                    ['/site/logout'],
+                                    ['data-method' => 'post', 'class' => 'btn btn-default btn-flat']
+                                ) ?>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+                <!-- User Account: style can be found in dropdown.less -->
+                <li>
+                    <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
