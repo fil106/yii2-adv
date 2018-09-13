d26bfeabea6afbe118ccad5e86de0b063e7ff363
Filipp Fedorov <fil106@mail.ru>
2018-09-13 23:45:42 +0300

    extend rest Controller, add methods

--- frontend/modules/api/controllers/ProjectController.php
+++ frontend/modules/api/controllers/ProjectController.php
@@ -10,11 +10,32 @@ class ProjectController extends Controller
 {
     public $modelClass = 'frontend\modules\api\models\Project';
 
-    public function actionIndex()
+    public function actions()
     {
-        return new ActiveDataProvider([
-            'query' => Project::find(),
-        ]);
+        return [
+            'index' => [
+                'class' => 'yii\rest\IndexAction',
+                'modelClass' => $this->modelClass,
+                'checkAccess' => [$this, 'checkAccess'],
+            ],
+            'view' => [
+                'class' => 'yii\rest\ViewAction',
+                'modelClass' => $this->modelClass,
+                'checkAccess' => [$this, 'checkAccess'],
+            ],
+        ];
+    }
+
+    protected function verbs()
+    {
+        return [
+            'index' => ['GET', 'HEAD'],
+            'view' => ['GET', 'HEAD'],
+        ];
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
     }
 
 }
